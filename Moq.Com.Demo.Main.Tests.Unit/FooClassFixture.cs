a13cb95f2a4497b7b6fb5fa7375020c39956b8d2
Andy Brown <andy@andygbrown.co.uk>
2018-04-15 18:45:45 +0100

    Add demo class in Main, and Fixture - and show that the fixture doesn't pass

---
+++ Moq.Com.Demo.Main.Tests.Unit/FooClassFixture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Autofac.Extras.Moq;
+using Microsoft.Office.Interop.Excel;
+using NUnit.Framework;
+
+namespace Moq.Com.Demo
+{
+    [TestFixture] public class FooClassFixture
+    {
+        public enum AssertionTestInput { AssertActual, VerifyMock }
+
+        [Test]
+        public void DoWork(
+            [Values] AssertionTestInput assertion)
+        {
+            DoWork<FooClass>(assertion);
+        }
+
+        public void DoWork<T>(AssertionTestInput assertion)
+            where T : IFooClass
+        {
+            //    NUnit test, edited for brevity
+            using (var mockFactory = AutoMock.GetLoose())
+            {
+                var expected = Guid.NewGuid().ToString();
+                mockFactory.Mock<Application>()
+                           .SetupGet(x => x.ActivePrinter)
+                           .Returns(expected);
+
+                var sut = mockFactory.Create<T>();
+
+                var actual = sut.DoWork();
+
+                //  AssertActual & ExpectFalse will always pass as mocks return false even without setups
+                if (assertion == AssertionTestInput.AssertActual)
+                    Assert.That(actual, Is.EqualTo(expected));
+                else
+                    mockFactory.Mock<Application>().VerifyGet(x => x.ActivePrinter, Times.Once);
+            }
+        }
+    }
+}
---
+++ Moq.Com.Demo.Main/FooClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Moq.Com.Demo
+{
+    public interface IFooClass
+    {
+        string DoWork();
+    }
+
+    public class FooClass : IFooClass
+    {
+        private readonly Application _comBarClass;
+
+        public FooClass(Application comBarClass)
+        {
+            _comBarClass = comBarClass;
+        }
+
+        public string DoWork()
+        {
+            return _comBarClass.ActivePrinter;
+        }
+    }
+}
