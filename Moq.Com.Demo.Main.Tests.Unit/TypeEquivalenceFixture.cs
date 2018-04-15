0766466e59bb6ffd9b6b192c0bc87fa76c8a31db
Andy Brown <andy@andygbrown.co.uk>
2018-04-15 17:50:21 +0100

    Add type equivalence test.
    - If you set Embed Interop Types to true then you get
    
    ```
    Error CS1759: Cannot embed interop types from assembly 'Moq.Com.Demo\packages\Microsoft.VisualStudio.OLE.Interop.7.10.6071\lib\Microsoft.VisualStudio.OLE.Interop.dll' #because it is missing either the 'ImportedFromTypeLibAttribute' attribute or the 'PrimaryInteropAssemblyAttribute' attribute
    ```

---
+++ Moq.Com.Demo.Main.Tests.Unit/TypeEquivalenceFixture.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.OLE.Interop;
+using NUnit.Framework;
+
+namespace Moq.Com.Demo
+{
+    [TestFixture]
+    public class TypeEquivalenceFixture
+    {
+        [Test]
+        public void TypesAreEquivalent()
+        {
+            var testDllType = typeof(IBinding);
+            var mainDllType = BindingWrapper.WrappedType();
+
+            //  ASSERT --------------------------------------------------------
+            Assert.That(mainDllType, Is.EqualTo(testDllType));
+        }
+    }
+}
---
+++ Moq.Com.Demo.Main/BindingWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.OLE.Interop;
+
+namespace Moq.Com.Demo
+{
+    public class BindingWrapper
+    {
+        public static Type WrappedType()
+        {
+            return typeof(IBinding);
+        }
+    }
+}
